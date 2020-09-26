f7cba2e1cdfb80be4d15bb8016ec5df70ffe6816
Leandro Liggerini <liggerinileo@gmail.com>
2020-09-26 02:01:10 -0300

    Updates

---
+++ precache-manifest.8a269ae8b09861a68bf5676de57f4c1c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb95f7117ec59ad13e78",
+    "url": "/PlatziBadges/static/css/main.12784886.chunk.css"
+  },
+  {
+    "revision": "cb95f7117ec59ad13e78",
+    "url": "/PlatziBadges/static/js/main.cb95f711.chunk.js"
+  },
+  {
+    "revision": "74875d496e47ad152b40",
+    "url": "/PlatziBadges/static/js/runtime~main.74875d49.js"
+  },
+  {
+    "revision": "b351806a5d07af171b63",
+    "url": "/PlatziBadges/static/css/2.da1f32af.chunk.css"
+  },
+  {
+    "revision": "b351806a5d07af171b63",
+    "url": "/PlatziBadges/static/js/2.b351806a.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/PlatziBadges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/PlatziBadges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/PlatziBadges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/PlatziBadges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/PlatziBadges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "8d21bc6703264f640ab55ee2402822d0",
+    "url": "/PlatziBadges/index.html"
+  }
+];
